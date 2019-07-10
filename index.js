2e02eb32579fb8df8b8629328c78898ac1390d4f
Samuel Desconsi <samuel.desconsi@melhorenvio.com>
2019-07-10 19:06:49 -0300

    Init

---
+++ index.js
@@ -0,0 +1,27 @@
+import messages from './messages/pt-br'
+
+export default validation => {
+  let errors = []
+
+  let params = validation.$params
+
+  Object.keys(params).map(type => {
+  	if (!validation[type]) {
+      console.log(Object.keys(messages).includes(type), Object.keys(messages), type)
+
+    	if (Object.keys(messages).includes(type)) {
+      	if (typeof messages[type] === 'function') {
+          errors.push(messages[type](params[type]))
+          return
+        }
+
+        errors.push(messages[type])
+        return
+      }
+
+     	errors.push('Campo incorreto')
+    }
+  })
+
+  return errors
+}
---
+++ messages/pt-br.js
@@ -0,0 +1,22 @@
+const invalid = `Campo inválido`
+
+export default {
+  required: `Este campo é obrigatório`,
+  requiredIf: params => params.prop && typeof params.prop !== 'function' ? `Obrigatório se o campo '${params.prop}' estiver preenchido` : invalid,
+  requiredUnless: params => params.prop && typeof params.prop !== 'function' ? `Obrigatório se o campo '${params.prop}' não estiver preenchido` : invalid,
+  minLength: params => params.min ? `Tamanho mínimo de ${params.min} caracteres é obrigatório` : invalid,
+  maxLength: params => params.max ? `Tamanho máximo de ${params.max} foi excedido` : invalid,
+  minValue: params => params.min ? `Valor mínimo de ${params.min} é obrigatório` : invalid,
+  maxValue: params => params.max ? `Valor máximo de ${params.max} foi excedido` : invalid,
+  between: params => params.min && params.max ? `Tamanho do campo precisa ser entre ${params.min} e ${params.max}` : invalid,
+  alpha: `O campo só pode conter letras`,
+  alphaNum: `O campo só pode conter letras e números`,
+  numeric: `O campo deve ser um número`,
+  integer: `O campo deve ser um número inteiro`,
+  decimal: `O campo deve conter um valor decimal`,
+  email: `Informe um endereço de e-mail válido`,
+  ipAddress: `Informe um endereço IP válido`,
+  macAddress: `Informe um endereço MAC válido`,
+  sameAs: params => params.eq ? `Valor informado deve ser igual a ${params.eq}` : invalid,
+  url: `Informe uma URL válida`
+}
