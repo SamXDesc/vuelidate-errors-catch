914eac8b5efd5b3183dbbb972ef8475c7f3693c6
Samuel Desconsi <samuel.desconsi@melhorenvio.com>
2019-08-09 15:36:51 -0300

    1.0.19

--- src/index.js
+++ src/index.js
@@ -9,40 +9,62 @@ export default class Main {
 
     this.fields = this._fields()
 
-    this.language = this._language()
+    const language = this._language()
+
+    this.messages = Object.keys(languagePack).includes(language)
+      ? languagePack[language]
+      : languagePack[defaultLanguage]
+
+    Object.assign(this.messages, this.options.messages)
   }
 
   run(validation) {
     let errors = []
 
-    let messages = Object.keys(languagePack).includes(this.language)
-      ? languagePack[this.language]
-      : languagePack[defaultLanguage]
-
     if (validation.hasOwnProperty('$params')) {
       let params = validation.$params
 
-      Object.assign(messages, this.options.messages)
-
       Object.keys(params).map(type => {
-        if (!validation[type]) {
-          if (Object.keys(messages).includes(type)) {
-            let scopeField = this._scopeFields(params[type])
 
-            if (!isFunction(scopeField)) {
-              if (typeof messages[type] === 'function') {
-                errors.push(messages[type](scopeField))
-                return
-              }
+        // Check if message exist
+        if (this.messages.hasOwnProperty(type)) {
+          let scopeField = this._scopeFields(params[type])
 
-              errors.push(messages[type])
+          if (!isFunction(scopeField)) {
+
+            if (isFunction(this.messages[type])) {
+              console.log(type)
+
+              errors.push(this.messages[type](scopeField))
               return
             }
-          }
 
-          errors.push(messages['invalidField'])
+            errors.push(this.messages[type])
+            return
+          }
         }
+
+        // Else, set generic message
+        errors.push(this.messages['invalidField'])
       })
+
+      // Object.keys(params).map(type => {
+      //   if (!Object.keys(validation).includes(type)) {
+
+      //     console.log(validation, type)
+
+
+      //     // if (Object.keys(messages).includes(type)) {
+      //     //   console.log(params[type])
+
+
+
+      //     //
+      //     // }
+
+
+      //   }
+      // })
     }
 
     return errors
@@ -60,27 +82,24 @@ export default class Main {
     return fields
   }
 
-  _scopeFields (params) {
-    if (params) {
-      delete params.type
+  _scopeFields (data) {
+    if (data) {
+      delete data.type
 
-      if (Object.keys(params).length === 1) {
-        let parameter = Object.values(params)[0]
+      if (Object.keys(data).length === 1) {
 
-        switch (true) {
-          case typeof parameter !== 'function' && Object.keys(this.fields).includes(parameter):
-            if (this.fields.hasOwnProperty(parameter)) {
-              return this.fields[parameter]
-            }
+        let parameter = Object.values(data)[0]
 
-            break
-
-          default:
-            return parameter
+        if (!isFunction(parameter) && this.fields.hasOwnProperty(parameter)) {
+          return this.fields[parameter]
         }
+
+        console.log('fall in here')
+
+        return parameter
       }
     }
 
-    return params
+    return data
   }
 }
