5261d6630cf28251d6baf57c5f3008dab8a0226c
Samuel <samueldesconsi@gmail.com>
2020-04-28 16:59:34 -0300

    WIP
    Removendo lixo do código.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import Main from './src'
 
-console.log('SSTART');
-
 export default {
   install: (Vue, options) => {
     Vue.prototype.$extractor = validation => new Main(options).run(validation)
